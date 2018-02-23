b76c6fb0966126b0d0bb3e0f93026c2a26e604f8
hanrong <liuzheng625@live.com>
2018-02-23 19:07:39 +0800

    Commit regularly

--- app/src/main/java/com/ljp/androidarchitecture/repository/UserRepository.java
+++ app/src/main/java/com/ljp/androidarchitecture/repository/UserRepository.java
@@ -2,22 +2,47 @@ package com.ljp.androidarchitecture.repository;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
+import android.util.Log;
 
+import com.ljp.androidarchitecture.BuildConfig;
 import com.ljp.androidarchitecture.backend.Webservice;
 import com.ljp.androidarchitecture.pojo.User;
 
 import javax.inject.Inject;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class UserRepository {
     private Webservice webservice;
 
+    private static class MyLogger1 implements HttpLoggingInterceptor.Logger {
+        @Override
+        public void log(String message) {
+            if (BuildConfig.DEBUG) Log.i("HTTP_LOG", message);
+        }
+    }
+
     @Inject
-    public UserRepository(Webservice webservice) {
-        this.webservice = webservice;
+    public UserRepository() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor(new MyLogger1());
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://api.ftx1.com")
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .client(client)
+                .build();
+        webservice = retrofit.create(Webservice.class);
     }
 
     // ...
