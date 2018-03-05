eba8af128386a8e193bec777766306e0ab788a10
hanrong <liuzheng625@live.com>
2018-03-05 17:18:04 +0800

    Update MainActivity.java

--- app/src/main/java/com/ljp/androidarchitecture/MainActivity.java
+++ app/src/main/java/com/ljp/androidarchitecture/MainActivity.java
@@ -21,14 +21,15 @@ public class MainActivity extends AppCompatActivity implements HasSupportFragmen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        UserProfileFragment userProfileFragment = new UserProfileFragment();
-        Bundle bundle = new Bundle();
-        bundle.putString(UserProfileFragment.UID_KEY, "100");
-        userProfileFragment.setArguments(bundle);
-        fragmentTransaction.replace(R.id.frameLayout, userProfileFragment);
-        fragmentTransaction.commit();
+        if (savedInstanceState == null) {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            UserProfileFragment userProfileFragment = new UserProfileFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString(UserProfileFragment.UID_KEY, "100");
+            userProfileFragment.setArguments(bundle);
+            fragmentTransaction.replace(R.id.frameLayout, userProfileFragment);
+            fragmentTransaction.commit();
+        }
     }
 
 
